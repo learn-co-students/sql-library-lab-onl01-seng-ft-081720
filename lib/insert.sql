3e7c3d418f8c5603edbf46e241773c3834070fc2
alexucontreras <alex.code29@gmail.com>
2020-09-19 21:53:47 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO series (author_id) VALUES (1),(1);
+
+INSERT INTO books (year) VALUES (1873),(1999),(1999),(1999),(1999),(1999);
+
+INSERT INTO characters (author_id) VALUES (1),(1),(1),(1),(1),(1),(1),(1);
+
+INSERT INTO subgenres (name) VALUES ('fiction'),('nonfiction');
+
+INSERT INTO authors (name) VALUES ('Me'),('You');
+
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1),(1, 1);
